15fd799199b90d7b2178dbb9a5c2c9ba71e51348
marounsaadeh <122361639+marounsaadeh@users.noreply.github.com>
2024-09-11 13:07:47 +0300

    Update FastAPI.py

--- FastAPI.py
+++ FastAPI.py
@@ -1,19 +1,16 @@
-# from fastapi import FastAPI
-# from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-# app = FastAPI()
+app = FastAPI()
 
-# class NameRequest(BaseModel):
-#     name: str
+class NameRequest(BaseModel):
+    name: str
 
-# @app.post("/print-name/")
-# async def print_name(request: NameRequest):
-#     print(request.name)  # This will print the name to the server's console
-#     return {"message": f"Name received: {request.name}"}
+@app.post("/print-name/")
+async def print_name(request: NameRequest):
+    print(request.name)  # This will print the name to the server's console
+    return {"message": f"Name received: {request.name}"}
 
-# if __name__ == '__main__':
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=5051)
-
-
-print("hello")
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5051)
